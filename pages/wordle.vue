c1d1afd77ac1ab59278446e375af14cc6975f701
Grant Erickson <grant@granterickson.com>
2022-03-22 07:24:55 -0700

    Fixed break points on Wordle Added simple transition

--- pages/wordle.vue
+++ pages/wordle.vue
@@ -1,54 +1,61 @@
 <template>
   <v-row justify="center">
-    <v-col cols="12" xs="8" sm="6" md="4" lg="3">
-      <v-card>
-        <v-card-title class="headline">
-          <div class="text-center">Guesses</div>
-        </v-card-title>
-        <v-card-text>
-          <v-row dense v-for="guess in guesses" :key="guess.word" class="my-4">
+    <v-col cols="12" xs="10" sm="9" md="7" lg="5">
+      <v-slide-y-reverse-transition>
+        <v-card v-show="expand">
+          <v-card-title class="headline">
+            <div class="text-center">Guesses</div>
+          </v-card-title>
+          <v-card-text>
+            <v-row
+              dense
+              v-for="guess in guesses"
+              :key="guess.word"
+              class="my-4"
+            >
+              <v-spacer />
+              <letter
+                v-for="(letter, index) in guess.letters"
+                :letter="letter"
+                :key="index"
+              ></letter>
+              <v-spacer />
+            </v-row>
+          </v-card-text>
+
+          <v-divider>inset</v-divider>
+
+          <v-card-text>
+            <v-row>
+              <letter
+                v-for="(letter, index) in word.letters"
+                :letter="letter"
+                :key="index"
+              ></letter>
+            </v-row>
+          </v-card-text>
+
+          <v-divider>inset</v-divider>
+
+          <v-card-text>
+            <v-row
+              dense
+              v-for="(keyRow, rowIndex) in keyboard.keyRows"
+              :key="rowIndex"
+            >
+              <letter
+                v-for="(letter, index) in keyRow"
+                :letter="letter"
+                :key="index"
+              ></letter>
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
             <v-spacer />
-            <letter
-              v-for="(letter, index) in guess.letters"
-              :letter="letter"
-              :key="index"
-            ></letter>
-            <v-spacer />
-          </v-row>
-        </v-card-text>
-
-<v-divider>inset</v-divider>
-
-        <v-card-text>
-          <v-row>
-            <letter
-              v-for="(letter, index) in word.letters"
-              :letter="letter"
-              :key="index"
-            ></letter>
-          </v-row>
-        </v-card-text>
-
-<v-divider>inset</v-divider>
-
-        <v-card-text>  
-          <v-row
-            dense
-            v-for="(keyRow, rowIndex) in keyboard.keyRows"
-            :key="rowIndex"
-          >
-            <letter
-              v-for="(letter, index) in keyRow"
-              :letter="letter"
-              :key="index"
-            ></letter>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="accent" nuxt to="/"> Home </v-btn>
-        </v-card-actions>
-      </v-card>
+            <v-btn color="accent" nuxt to="/"> Home </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-slide-y-reverse-transition>
     </v-col>
   </v-row>
 </template>
@@ -63,12 +70,19 @@ export default class WordlePage extends Vue {
   word: Word = new Word('.....')
   guesses: Word[] = []
   keyboard: Keyboard = new Keyboard(KeyboardLanguage.English)
+  expand: boolean = false
 
   constructor() {
     super()
     this.guesses.push(new Word('PEACH', '01320'))
     this.guesses.push(new Word('DIRTY', '00321'))
     this.guesses.push(new Word('SOUND', '32010'))
+  }
+
+  mounted() {
+    setTimeout(() => {
+      this.expand = true
+    }, 500)
   }
 }
 </script>
